afa502b5abd07914041e4ba2727791c9c5eae43e
Nate <nathan.quan@uconn.edu>
2023-07-25 21:27:46 -0400

    completed 1, 2, 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let ori_list = string.split('');
+    let new_list = [];
 
+    for (let i = 1; i <= ori_list.length; i++) {
+        new_list.push(ori_list[ori_list.length - i]);
+    }
+    let new_string = '';
+    for (let i = 0; i < new_list.length; i++){
+        new_string += new_list[i];
+    }
+    return new_string;
 };
 
 // Do not edit below this line
