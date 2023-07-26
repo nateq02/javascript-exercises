b69131cf75423bfb8ba0e7b74d5f84920ffe143b
Nate <nathan.quan@uconn.edu>
2023-07-25 21:42:48 -0400

    completed 4, 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // Makes a list of arguments to remove from arr
+    let toRemove = [...args];
 
+    for (let i = 0; i < toRemove.length; i++) {
+        for (let j = 0; j < arr.length; j++){
+            if (args[i] === arr[j]){
+                arr.splice(j, 1);
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
