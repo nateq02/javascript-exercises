f8d1aa69131030f6cfc3223c29d8860b0750e2b5
Nate <nathan.quan@uconn.edu>
2023-07-26 21:17:39 -0400

    completed 6, 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let new_temp = ((temp - 32)*5)/9;
+    let round_temp = new_temp.toFixed(1);
+    return parseFloat(round_temp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let new_temp = (temp * 9 / 5) + 32;
+    let round_temp = new_temp.toFixed(1);
+    return parseFloat(round_temp);
 };
 
 // Do not edit below this line
